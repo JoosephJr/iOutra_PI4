61a86a9fb22ea476ab241cac9f2a875416c0e4d5
JoosephJr <jose.rlsjunior@gmail.com>
2024-09-20 17:42:09 -0300

    V2.2
    Sprint 1 Correção Confirmar Senha, CPF, inativar/ativar usuário
    ------------------------------
    Sprint 2 Cadastro de Produto com uma imagem, página de detalhe, ativação e inativação de produto

---
+++ iOutra/src/main/java/com/example/iOutra/controller/UsuarioController.java
@@ -0,0 +1,145 @@
+package com.example.iOutra.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.iOutra.model.Usuario;
+import com.example.iOutra.model.UsuarioDTO;
+import com.example.iOutra.repository.UsuarioRepository;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository userRepo;
+
+    @GetMapping({ "", "/" })
+    public String exibirListaUsuarios(Model model) {
+        List<Usuario> usuarios = userRepo.findAll(Sort.by(Sort.Direction.DESC, "idUsuario"));
+        model.addAttribute("usuarios", usuarios);
+        return "usuarios/index";
+    }
+
+    @GetMapping("/cadastrarUsuario")
+    public String exibirCadastrarUsuario(Model model) {
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        model.addAttribute("usuarioDTO", usuarioDTO);
+        return "usuarios/cadastrarUsuario";
+    }
+
+    @PostMapping("/cadastrarUsuario")
+    public String cadastrarUsuario(@Valid @ModelAttribute UsuarioDTO usuarioDTO,
+            BindingResult result) {
+
+        if (userRepo.findByEmail(usuarioDTO.getEmail()) != null) {
+            result.addError(new FieldError("usuarioDTO", "email", usuarioDTO.getEmail(),
+                    false, null, null, "Endereço de e-mail já está sendo utilizado!"));
+        }
+
+        if (result.hasErrors()) {
+            return "usuarios/cadastrarUsuario";
+        }
+
+        if (!usuarioDTO.getSenha().equals(usuarioDTO.getConfirmaSenha())) {
+            result.addError(new FieldError("usuarioDTO", "senha", "As senhas não são iguais!"));
+        }
+
+        if (result.hasErrors()) {
+            return "usuarios/cadastrarUsuario";
+        }
+
+        Usuario usuario = new Usuario();
+        usuario.setNome(usuarioDTO.getNome());
+        usuario.setEmail(usuarioDTO.getEmail());
+        usuario.setCpf(usuarioDTO.getCpf());
+        usuario.setGrupo(usuarioDTO.getGrupo());
+        usuario.setSenha(usuarioDTO.getSenha());
+
+        if (!usuarioDTO.getSenha().equals(usuarioDTO.getConfirmaSenha())) {
+            result.addError(new FieldError("usuarioDTO", "senha", "As senhas não são iguais!"));
+        }
+
+        if (result.hasErrors()) {
+            return "usuarios/cadastrarUsuario";
+        }
+
+        userRepo.save(usuario);
+
+        return "redirect:/usuarios";
+    }
+
+    @GetMapping("/editarUsuario")
+    public String exibirEditarUsuario(Model model, @RequestParam int idUsuario) {
+        Usuario usuario = userRepo.findById(idUsuario).get();
+
+        if (usuario == null) {
+            return "redirect:/usuarios";
+        }
+
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setNome(usuario.getNome());
+        usuarioDTO.setEmail(usuario.getEmail());
+        usuarioDTO.setCpf(usuario.getCpf());
+        usuarioDTO.setGrupo(usuario.getGrupo());
+        usuarioDTO.setSenha(usuario.getSenha());
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("usuarioDTO", usuarioDTO);
+
+        return "usuarios/editarUsuario";
+    }
+
+    @PostMapping("/editarUsuario")
+    public String editarUsuario(Model model, @RequestParam int idUsuario,
+            @Valid @ModelAttribute UsuarioDTO usuarioDTO, BindingResult result) {
+
+        Usuario usuario = userRepo.findById(idUsuario).orElse(null);
+        if (usuario == null) {
+            return "redirect:/usuarios";
+        }
+
+        model.addAttribute("usuario", usuario);
+
+        if (result.hasErrors()) {
+            System.out.println(result.getAllErrors());
+            return "usuarios/editarUsuario";
+        }
+
+        usuario.setNome(usuarioDTO.getNome());
+        usuario.setCpf(usuarioDTO.getCpf());
+        usuario.setGrupo(usuarioDTO.getGrupo());
+
+        // Atualiza a senha somente se for preenchida
+        if (usuarioDTO.getSenha() != null && !usuarioDTO.getSenha().isEmpty()) {
+            usuario.setSenha(usuarioDTO.getSenha());
+        }
+        try {
+            userRepo.save(usuario);
+
+        } catch (Exception e) {
+            // talvez mexer aqui pra pegar exceção melhor
+            System.out.println(e);
+        }
+
+        return "redirect:/usuarios";
+    }
+
+    @PostMapping("/alterarStatus")
+    public String alterarStatus(@RequestParam int id) {
+        Usuario usuario = userRepo.findById(id).orElseThrow();
+        usuario.setActive(!usuario.isActive());
+        userRepo.save(usuario);
+        return "redirect:/usuarios";
+    }
+
+}
---
+++ iOutra/src/main/java/com/example/iOutra/model/Usuario.java
@@ -0,0 +1,116 @@
+package com.example.iOutra.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+    
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int idUsuario;
+
+@NotBlank (message = "O preenchimento do nome é obrigatório!")
+private String nome;
+
+@Column(unique = true)
+@NotBlank (message = "O preenchimento do CPF é obrigatório!")
+@Size(min = 11, max = 11, message = "O CPF deve conter 11 dígitos.")
+private String cpf;
+
+@Column(unique = true, nullable = false)
+@Email (message = "Formato de e-mail inválido!")
+private String email;
+
+private String grupo;
+
+private boolean isActive = true;
+
+// private String status; //definir se vou usar essa ou boolean    
+
+@NotEmpty(message = "A senha é obrigatória!")
+@Size(min = 4, max = 20, message = "A senha deve conter entre 4 e 20 caracteres!")
+private String senha;
+
+// @NotEmpty(message = "A senha é obrigatória!")
+// @Size(min = 4, max = 20, message = "A senha deve conter entre 4 e 20 caracteres!")
+// private String confirmaSenha;
+
+public int getIdUsuario() {
+    return idUsuario;
+}
+
+public void setIdUsuario(int idUsuario) {
+    this.idUsuario = idUsuario;
+}
+
+public String getNome() {
+    return nome;
+}
+
+public void setNome(String nome) {
+    this.nome = nome;
+}
+
+public String getCpf() {
+    return cpf;
+}
+
+public void setCpf(String cpf) {
+    this.cpf = cpf;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
+
+public String getGrupo() {
+    return grupo;
+}
+
+public void setGrupo(String grupo) {
+    this.grupo = grupo;
+}
+
+public boolean isActive() {
+    return isActive;
+}
+
+public void setActive(boolean isActive) {
+    this.isActive = isActive;
+}
+
+// public String getStatus() {
+//     return status;
+// }
+
+// public void setStatus(String status) {
+//     this.status = status;
+// }
+
+public String getSenha() {
+    return senha;
+}
+
+public void setSenha(String senha) {
+    this.senha = senha;
+}
+
+// public String getConfirmaSenha() {
+//     return confirmaSenha;
+// }
+
+// public void setConfirmaSenha(String confirmaSenha) {
+//     this.confirmaSenha = confirmaSenha;
+}
+
+
+
+
