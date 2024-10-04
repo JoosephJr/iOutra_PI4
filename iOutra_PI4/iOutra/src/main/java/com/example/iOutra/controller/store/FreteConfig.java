681e98ec017be309b495616f6a607050ab5d5c81
Tiago Rabello <tiago.rabellobraz@gmail.com>
2024-10-04 14:15:10 -0300

    Create FreteConfig.java

---
+++ iOutra_PI4/iOutra/src/main/java/com/example/iOutra/controller/store/FreteConfig.java
@@ -0,0 +1,43 @@
+package com.example.iOutra.controller.store;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+import com.example.iOutra.model.Frete;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class FreteConfig {
+
+    private BigDecimal valor;
+
+    public FreteConfig() {
+        generateRandomValue();
+    }
+
+    private void generateRandomValue() {
+        Random random = new Random();
+        valor = BigDecimal.valueOf(9 + (30 - 9) * random.nextDouble());
+    }
+
+    @Bean
+    public Frete freteExpresso() {
+        return new Frete(valor, "Expresso", valor, 3);
+    }
+
+
+    @Bean
+    public Frete freteEconomico() {
+        BigDecimal valorComDesconto = valor.multiply(BigDecimal.valueOf(0.8)); // 20% discount
+        return new Frete(valorComDesconto, "Econômico", valorComDesconto, 8);
+    }
+
+    @Bean
+    public Frete fretePadrao() {
+        BigDecimal valorComDesconto = valor.multiply(BigDecimal.valueOf(0.9)); // 10% discount
+        return new Frete(valorComDesconto, "Padrão", valorComDesconto, 5);
+    }
+}
