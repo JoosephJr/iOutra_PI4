9811a011f94bc522f84dbdb17a9c73bfc63013e8
Rafael Machado <rmr.eu@hotmail.com.br>
2024-11-13 15:23:38 -0300

    aa

---
+++ iOutra_PI4-user-session-test/iOutra_PI4/iOutra/src/main/java/com/example/iOutra/infra/security/SecurityConfiguration.java
@@ -0,0 +1,73 @@
+package com.example.iOutra.infra.security;
+
+import com.example.iOutra.service.MyUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    MyUserDetailService myUserRepository;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> {
+
+                    /* Todos */
+                    registry.requestMatchers("/resumo", "/resumo/**", "/pagamento", "/pagamento/**","/addEndereco", "/setup", "/img/**", "/css/**", "/css", "/backoffice/setup", "/", "/produto", "/login", "/cadastro", "/cadastro/**","/sair", "/carrinho", "/item", "/item/**", "/itemreduzir", "/remover","/pedidos", "/pedidos/**", "/comprar").permitAll();
+
+                    /* Administrador e Estoquista */
+                    registry.requestMatchers("/backoffice/", "/backoffice/pedidos","/backoffice/produto/**","/backoffice/usuario/**", "/backoffice/pedidos/**").permitAll();
+                    /* Estoquista ---> NÃO PODE ACESSAR /backoffice/listar-produtos */
+                    registry.requestMatchers("/backoffice/usuarios").permitAll();
+
+                    registry.anyRequest().authenticated();
+                })
+                .formLogin(httpSecurityFormLoginConfigurer -> {
+                    httpSecurityFormLoginConfigurer
+                            .loginPage("/backoffice")
+                            .usernameParameter("email")
+                            .successHandler(new SimpleUrlAuthenticationSuccessHandler("/backoffice/home"))
+                            .permitAll();
+                })
+                .logout(logoutConfigurer -> logoutConfigurer
+                        .logoutSuccessUrl("/backoffice")
+                        .logoutUrl("/logout")
+                        .permitAll())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return myUserRepository;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(myUserRepository);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
